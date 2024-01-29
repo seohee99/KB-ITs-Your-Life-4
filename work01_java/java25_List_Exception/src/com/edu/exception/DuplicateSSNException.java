c009b8680c8d17ca40a7f6a09032bde6d67055e7
seohee <53520867+seohee99@users.noreply.github.com>
2024-01-29 11:30:32 +0900

    [JAVA] Exception 2

---
+++ work01_java/java25_List_Exception/src/com/edu/exception/DuplicateSSNException.java
@@ -0,0 +1,11 @@
+package com.edu.exception;
+
+public class DuplicateSSNException extends Exception {
+	public DuplicateSSNException() {
+		this("This is DuplicateSSNException...");
+	}
+	
+	public DuplicateSSNException(String message) {
+		super(message);
+	}
+}
