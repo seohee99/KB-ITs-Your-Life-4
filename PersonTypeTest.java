841542feb81016a8545213776aa1b1d34fd602a1
seohee <53520867+seohee99@users.noreply.github.com>
2024-01-29 10:41:12 +0900

    [JAVA] PersonTypeTest.java

---
+++ PersonTypeTest.java
@@ -0,0 +1,26 @@
+package com.edu.test;
+import java.util.Scanner;
+
+/*
+ <<입력 Data Sample >>
+ 6
+ 1 4 4 4 5 3
+ 
+ */
+public class PersonTypeTest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		  System.out.println("Sample Data Input >>> ");
+
+
+		  
+		  // 코드 구현
+		  
+		  
+		  
+		  System.out.println("가장 많은 타입의 유형은 "+answer+" 타입입니다.!!");
+		  
+		 }
+		}
+
